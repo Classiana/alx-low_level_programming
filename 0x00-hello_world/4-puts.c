a02ae1d7f58410acf9ca680c62f97bc8df8de451
Classiana <junnydesina406@gmail.com>
2023-09-07 05:40:38 -0700

    Updating

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#!/bin/bash
+#1/bin/bash
 #include <stdio.h>
+
 /**
-* main - Entry point
-*
-* Return: always 0
-*/ 
+* main -Entry point
+* Return: Always 0
+*/
 int main(void)
-{ 
-    puts("\" Programming is like building a multilingual puzzle");
-    return (0);
+{
+     puts("Programming is like building a multilingual puzzle"\n);
+     return (0);
 }
