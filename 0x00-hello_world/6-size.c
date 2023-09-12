dac7e4a25da14d74624579b58eeae6a0d53f0004
Classiana <junnydesina406@gmail.com>
2023-09-12 04:12:01 -0700

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#!/bin/bash
+#include <stdio.h>
 
 /**
  * main - prints the size of various types
@@ -12,4 +12,3 @@ int main(void)
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
-}
