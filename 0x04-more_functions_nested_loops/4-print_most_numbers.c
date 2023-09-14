adef68e6a415b95d0beba2bbd54f29ae4c87d91e
Classiana <junnydesina406@gmail.com>
2023-09-14 12:03:06 -0700

    printing out numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,23 @@
 #include "holberton.h"
 
 /**
- * print_numbers - prints 0 to 9.
+ * print_most_numbers -  checks for checks for a digit (0 through 9).
  *
+ * Return: Always 0.
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
-	int num = 0;
+	int a;
 
-	while (num < 10)
+	for (a = 48; a < 58; a++)
 	{
-		_putchar('0' + num);
-		num++;
+		if (a != 50)
+		{
+			if (a != 52)
+			{
+			_putchar(a);
+			}
+		}
 	}
 	_putchar('\n');
 }
