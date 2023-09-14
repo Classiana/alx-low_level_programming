3a10f8b64f14eb2925f5036e0fde083c4f5365b2
Classiana <junnydesina406@gmail.com>
2023-09-14 08:46:32 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Description: printing the numbers, from 0 to 9, followed by a new line.
+ * Return: Always 0.
+ */
+void print_numbers(void);
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		putchar(m);
+putchar('\n');
+return (0);
+}
